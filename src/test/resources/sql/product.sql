e8098418f838a4faa608e913e42ebf1a2ff80c0e
大漠真人 <good-long@qq.com>
2013-11-20 23:06:05 +0800

    Signed-off-by: 大漠真人 <good-long@qq.com>

--- src/test/resources/sql/product.sql
+++
@@ -1,9 +0,0 @@
-TRUNCATE TABLE `product`;
-INSERT INTO `product` (`id`, `product_type_id`, `product_name`, `product_code`, `price`, `description`) VALUES ('1', '1', 'iPhone 3g', 'MP001', '3000', 'iPhone 3g 移动电话');
-INSERT INTO `product` (`id`, `product_type_id`, `product_name`, `product_code`, `price`, `description`) VALUES ('2', '1', 'iPhone 3gs', 'MP002', '3500', 'iPhone 3gs 移动电话');
-INSERT INTO `product` (`id`, `product_type_id`, `product_name`, `product_code`, `price`, `description`) VALUES ('3', '2', 'iPad 2', 'TC001', '3000', 'iPad 2 平板电脑');
-INSERT INTO `product` (`id`, `product_type_id`, `product_name`, `product_code`, `price`, `description`) VALUES ('4', '1', 'iPhone 4', 'MP003', '4000', 'iPhone 4 移动电话');
-INSERT INTO `product` (`id`, `product_type_id`, `product_name`, `product_code`, `price`, `description`) VALUES ('5', '1', 'iPhone 4s', 'MP004', '4500', 'iPhone 4s 移动电话');
-INSERT INTO `product` (`id`, `product_type_id`, `product_name`, `product_code`, `price`, `description`) VALUES ('6', '2', 'iPad 3', 'TC002', '3000', 'iPad 3 平板电脑');
-INSERT INTO `product` (`id`, `product_type_id`, `product_name`, `product_code`, `price`, `description`) VALUES ('7', '1', 'iPhone 5', 'MP005', '5000', 'iPhone 5 移动电话');
-INSERT INTO `product` (`id`, `product_type_id`, `product_name`, `product_code`, `price`, `description`) VALUES ('8', '2', 'iPad mini', 'TC003', '3000', 'iPad mini 平板电脑');
