5106e17ef97f513d3472622851b7c28dd32fabc7
黄勇 <huangyong.java@gmail.com>
2013-12-18 00:35:29 +0800

    【I】简化注解配置，Service 与 Action 无需继承 Base 类

--- src/main/java/com/smart/bootstrap/action/SystemAction.java
+++ src/main/java/com/smart/bootstrap/action/SystemAction.java
@@ -1,21 +1,20 @@
 package com.smart.bootstrap.action;
 
 import com.smart.framework.DataContext;
-import com.smart.framework.annotation.Bean;
+import com.smart.framework.annotation.Action;
 import com.smart.framework.annotation.Request;
-import com.smart.framework.base.BaseAction;
 import com.smart.framework.bean.Page;
 import com.smart.framework.bean.Result;
 
-@Bean
-public class SystemAction extends BaseAction {
+@Action
+public class SystemAction {
 
-    @Request("post:/login")
+    @Request("POST:/login")
     public Page login() {
         return new Page("/welcome");
     }
 
-    @Request("get:/logout")
+    @Request("GET:/logout")
     public Result logout() {
         DataContext.Session.removeAll();
         return new Result(true);
