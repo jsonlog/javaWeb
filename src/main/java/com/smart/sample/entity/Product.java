b42c01b91daa5dd224e350f391d68fc50b7917a9
黄勇 <huangyong.java@gmail.com>
2014-02-09 01:01:25 +0800

    first commit

---
+++ src/main/java/com/smart/sample/entity/Product.java
@@ -0,0 +1,74 @@
+package com.smart.sample.entity;
+
+import com.smart.framework.base.BaseEntity;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Product extends BaseEntity {
+
+    private long productTypeId;
+
+    private String name;
+
+    private String code;
+
+    private int price;
+
+    private String description;
+
+    private String picture;
+
+    public Product() {
+    }
+
+    public long getProductTypeId() {
+        return productTypeId;
+    }
+
+    public void setProductTypeId(long productTypeId) {
+        this.productTypeId = productTypeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+}
