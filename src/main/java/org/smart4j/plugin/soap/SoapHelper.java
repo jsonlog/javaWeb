60916d57b198d00ccc00ed4907017f4f6d9f3376
黄勇 <huangyong.java@gmail.com>
2014-06-17 09:55:44 +0800

    【I】添加 ws client 方法

--- src/main/java/org/smart4j/plugin/soap/SoapHelper.java
+++ src/main/java/org/smart4j/plugin/soap/SoapHelper.java
@@ -2,10 +2,13 @@ package org.smart4j.plugin.soap;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.frontend.ClientProxyFactoryBean;
 import org.apache.cxf.frontend.ServerFactoryBean;
 import org.apache.cxf.interceptor.Interceptor;
 import org.apache.cxf.interceptor.LoggingInInterceptor;
 import org.apache.cxf.interceptor.LoggingOutInterceptor;
+import org.apache.cxf.jaxws.endpoint.dynamic.JaxWsDynamicClientFactory;
 import org.apache.cxf.message.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -45,5 +48,36 @@ public class SoapHelper {
         factory.setOutInterceptors(outInterceptorList);
         factory.create();
         logger.debug("Publish SOAP Service: " + interfaceClass.getName());
+    }
+
+    /**
+     * 创建 SOAP 客户端
+     */
+    public static <T> T createClient(String wsdl, Class<? extends T> interfaceClass) {
+        ClientProxyFactoryBean factory = new ClientProxyFactoryBean();
+        factory.setAddress(wsdl);
+        factory.setServiceClass(interfaceClass);
+        factory.setInInterceptors(inInterceptorList);
+        factory.setOutInterceptors(outInterceptorList);
+        return factory.create(interfaceClass);
+    }
+
+    /**
+     * 调用 SOAP WS 操作
+     *
+     * @since 1.1
+     */
+    public static Object[] invokeOperation(String wsdl, String operationName, Object... operationParams) {
+        Object[] results = null;
+        JaxWsDynamicClientFactory factory = JaxWsDynamicClientFactory.newInstance();
+        Client client = factory.createClient(wsdl);
+        if (client != null) {
+            try {
+                results = client.invoke(operationName, operationParams);
+            } catch (Exception e) {
+                logger.error("调用 SOAP WS 操作失败！", e);
+            }
+        }
+        return results;
     }
 }
