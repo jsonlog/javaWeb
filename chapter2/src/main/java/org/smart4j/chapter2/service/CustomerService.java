d9c22fd86f0a50410a63c94262b552031f9ae5b8
Olly <1208894193@qq.com>
2018-01-28 10:33:33 +0800

    提高方法

--- chapter2/src/main/java/org/smart4j/chapter2/service/CustomerService.java
+++ chapter2/src/main/java/org/smart4j/chapter2/service/CustomerService.java
@@ -1,7 +1,9 @@
 package org.smart4j.chapter2.service;
 
+import javafx.scene.chart.PieChart;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.smart4j.chapter2.helper.DatabaseHelper;
 import org.smart4j.chapter2.model.Customer;
 import org.smart4j.chapter2.util.PropsUtil;
 
@@ -13,79 +15,33 @@ import java.util.Properties;
 
 public class CustomerService {
 
-    private static final Logger LOGGER;
-    private static final String DRIVER;
-    private static final String URL;
-    private static final String USERNAME;
-    private static final String PASSWORD;
+    private static final Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
 
-    static {
-        LOGGER = LoggerFactory.getLogger(CustomerService.class);
-        Properties conf = PropsUtil.loadProps("config.properties");
-        DRIVER = conf.getProperty("jdbc.driver");
-        URL = conf.getProperty("jdbc.url");
-        USERNAME = conf.getProperty("jdbc.username");
-        PASSWORD = conf.getProperty("jdbc.password");
-
-        try {
-            Class.forName(DRIVER);
-        }catch(ClassNotFoundException e) {
-            LOGGER.error("can not load jdbc driver" + e);
-        }
-    }
 
     public List<Customer> getCustomerList() {
-        Connection conn = null;
-        try {
-            List<Customer> customerList = new ArrayList<Customer>();
-            String sql = "SELECT * FROM customer";
-            conn = DriverManager.getConnection(URL,USERNAME,PASSWORD);
-            PreparedStatement stmt = conn.prepareStatement(sql);
-            ResultSet rs = stmt.executeQuery();
-            while(rs.next()) {
-                Customer customer = new Customer();
-                customer.setId(rs.getLong("id"));
-                customer.setName(rs.getString("name"));
-                customer.setContact(rs.getString("contact"));
-                customer.setTelephone(rs.getString("telephone"));
-                customer.setEmail(rs.getString("email"));
-                customer.setRemark(rs.getString("remark"));
-                customerList.add(customer);
 
-            }
-            return customerList;
-        }catch(SQLException e) {
-            LOGGER.error("execute sql failure",e);
-        }finally {
-            if(conn!=null) {
-                try {
-                    conn.close();
-                }catch(SQLException e) {
-                    LOGGER.error("close connection failure",e);
-                }
-            }
-        }
-        return null;
+        String sql = "SELECT * FROM customer";
+        return DatabaseHelper.queryEntityList(Customer.class,sql);
+
     }
 
     public Customer getCustomer(long id) {
-        // TODO
-        return null;
+        String sql = " SELECT * FROM customer";
+        return DatabaseHelper.queryEntity(Customer.class,sql);
     }
 
     public boolean createCustomer(Map<String,Object> fieldMap) {
-        // TODO
-        return false;
+        return DatabaseHelper.insertEntity(Customer.class,fieldMap);
     }
 
     public boolean updateCustomer(long id,Map<String,Object> fieldMap) {
-        // TODO
-        return false;
+
+        return DatabaseHelper.updateEntity(Customer.class,id,fieldMap);
     }
 
     public boolean deleteCustomer(long id) {
-        // TODO
-        return false;
+
+        return DatabaseHelper.deleteEntity(Customer.class,id);
     }
 
 
--- chapter2/target/test-classes/init.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `demo`.`customer` (`id`, `name`, `contact`, `telephone`, `email`) VALUES ('1', 'customer1', 'Jack', '13512345678', 'jack@gmail.com');
-INSERT INTO `demo`.`customer` (`id`, `name`, `contact`, `telephone`, `email`, `remark`) VALUES ('2', 'customer2', 'Rose', '136123456789', 'rose@gmail.com', '');
